195b8f72cfdb843af9edf47de10a3b8b196d00d9
Maroua Cherki <cherkimaroua7@gmail.com>
2025-12-01 10:15:08 +0100

    Update RecipeForm1.jsx

--- RecipeForm1.jsx
+++ RecipeForm1.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 function RecipeForm(props) {
   const { recipe, onSubmit, onClose } = props;
   const [nom, setNom] = useState('');
@@ -159,4 +159,5 @@ function RecipeForm(props) {
 }
 
 export default RecipeForm;
+
 
